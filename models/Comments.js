9e40d7d14195fa57ba4f2b43ffd8c5a02ac73d9a
Dan Beck <danjacobbeck@gmail.com>
2022-01-08 13:50:43 -0800

    commit on main

--- models/Comments.js
+++ models/Comments.js
@@ -1,10 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {
+class Comment extends Model {
 }
 
-Blog.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -12,29 +12,33 @@ Blog.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     content: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    blogger_id: {
+    blog_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'user',
+        model: 'blog',
         key: 'id',
       },
     },
+
+    // user_id:{ 
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: 'user',
+    //     key: 'id',
+    //   },
+    // }
   },
   {
     sequelize,
     timestamps: true,
     freezeTableName: true,
     underscored: true,
-    modelName: 'blog',
+    modelName: 'comment',
   }
 );
 
-module.exports = Blog;
+module.exports = Comment;
