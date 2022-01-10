ab9031d77ec4e81d7e55d893c065b24d248793f1
Dan Beck <danjacobbeck@gmail.com>
2022-01-10 14:51:43 -0800

    commit on main

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const PORT = process.env.PORT || '8080'
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+app.set("port", PORT); 
 
 const hbs = exphbs.create({ helpers });
 
